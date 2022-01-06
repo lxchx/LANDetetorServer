b12b0ee4c101f43a04c0a97781e9e70c7603eb74
lxchx <lc.9816@163.com>
2022-01-06 22:40:56 +0800

    添加网段支持

--- tool/common.py
+++ tool/common.py
@@ -15,15 +15,30 @@ def ip_int(ip):
         res += 256**j * int(i)
     return res
 
+def ip_seg_ips(segment):
+    ip_seg, ip_band = segment.split('/', maxsplit=1)
+    ip_band=int(ip_band)
+    ip_seg=ip_int(ip_seg)
+    ips=list()
+    for i in range(ip_band+1):
+        ip_str=int_ip(ip_seg+i)
+        ips.append(ip_str)
+    return ips
 
 def make_response(obj):
     return {"msg": "成功", "data": obj}
 
 
-def detect(flags, ips, func):
+def detect(flags, detectors, func):
     print("detect Start")
     flags['is_detecting'] = True
     sapi = scamperApi()
+    ips=list()
+    for ip in detectors:
+        if '/' in detectors:
+            ips.extend(ip_seg_ips(ip))
+        else:
+            ips.append(ip)
     while flags['should_detect']:
         func(sapi.tracert(ips))
     flags['is_detecting'] = False
